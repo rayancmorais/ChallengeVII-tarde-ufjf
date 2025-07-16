564899ae050543f731de0f033962d2ee1880dd9b
Rayan Morais <rayan_cm2021@icloud.com>
2025-07-15 23:18:57 -0300

    adiciona netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'; // <--- ADICIONE ESTA LINHA
 import path from 'path';
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss() // <--- ADICIONE ESTA LINHA
+    tailwindcss(), // <--- ADICIONE ESTA LINHA
+    netlifyPlugin()
   ],
   resolve: {
     alias: {
